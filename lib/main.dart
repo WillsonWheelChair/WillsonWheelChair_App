c317824fdf515c4232a3e15f215fb1db0226f029
diadntjr <dntjr0501@gmail.com>
2022-07-21 14:13:37 +0900

    Update main.dart
    Change to init

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_kakao_map/flutter_kakao_map.dart';
-import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:willsonwheelchair_app/pages/home/home_view_page.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const MyAppOne(),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ScreenUtilInit(
+    designSize: const Size(358, 598),
+    builder: (context, child) => MaterialApp(
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          child: child!,
+        );
+      },
+      home: const HomeViewPage(),
+    ),
+  ));
 }
-
-class MyAppOne extends StatefulWidget {
-  const MyAppOne({Key? key}) : super(key: key);
-
-  @override
-  State<MyAppOne> createState() => _MyAppOneState();
-}
-
-class _MyAppOneState extends State<MyAppOne> {
-  late KakaoMapController mapController;
-  MapPoint _visibleRegion = const MapPoint(37.5087553, 127.0632877);
-  final CameraPosition _kInitialPosition =
-      const CameraPosition(target: MapPoint(37.5087553, 127.0632877), zoom: 5);
-
-  void onMapCreated(KakaoMapController controller) async {
-    final MapPoint visibleRegion = await controller.getMapCenterPoint();
-    setState(() {
-      mapController = controller;
-      _visibleRegion = visibleRegion;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Flutter KakoMap example'),),
-      body: Column(
-        children: [
-          Center(
-            child: SizedBox(
-              width: 300.0,
-              height: 200.0,
-              child: KakaoMap(
-                onMapCreated: onMapCreated,
-                initialCameraPosition: _kInitialPosition,
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-
