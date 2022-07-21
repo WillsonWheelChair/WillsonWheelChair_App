6d56ddead38131f0f6e637a9292862a1e76cf69a
diadntjr <dntjr0501@gmail.com>
2022-07-21 17:40:56 +0900

    Update main.dart
    Add GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 import 'firebase_options.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:willsonwheelchair_app/pages/home/home_view_page.dart';
@@ -11,13 +12,14 @@ void main() async {
   );
   runApp(ScreenUtilInit(
     designSize: const Size(358, 598),
-    builder: (context, child) => MaterialApp(
+    builder: (context, child) => GetMaterialApp(
       builder: (context, child) {
         return MediaQuery(
           data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
           child: child!,
         );
       },
+      locale: Get.locale,
       home: const HomeViewPage(),
     ),
   ));
